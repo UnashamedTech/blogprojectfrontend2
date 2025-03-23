2c9e9c6f8ac4ff94505e68ed91c26d42b2510663
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-03-23 11:11:59 +0300

    feat: implement user fetching API and add base API call functionality

---
+++ actions/users.ts
@@ -0,0 +1,15 @@
+"use server";
+
+// import apiCall from "@/base-api/api";
+
+// const BASE_URL = `` //define the base url here from .env
+
+export const fetchUsers = async () => {
+  // const endpoint = `${BASE_URL}/....` //endpoint of the specific user fetching url
+  // const response = await apiCall({url:endpoint}) // this is the api calling if the method is Get no need to add as a paramenter but
+  //if it is Post Patch or Delete u will add apicall({url:endpoint, Method:"DELETE"}) if there is addition of data
+  // like POST with data go for apiCall({url:endpoint, Method:"Post", data: data}) the data will be inside the async({data:type})
+  // then
+  //return response
+  // then call this and use it in the
+};
---
+++ base-api/api.ts
@@ -0,0 +1,60 @@
+"use server";
+
+interface CustomError {
+  message: string;
+  response: {
+    data: unknown;
+  };
+}
+
+export default async function apiCall({
+  url,
+  method = "GET",
+  data = [],
+}: {
+  url: string;
+  method?: string;
+  data?: unknown;
+}) {
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "", // Add token here if needed `Bearer ${token || ""}`
+      },
+      body: method !== "GET" ? JSON.stringify(data) : undefined,
+    });
+    // Check if the response is JSON
+    const contentType = response.headers.get("Content-Type");
+    if (contentType && contentType.includes("application/json")) {
+      const responseData = await response.json();
+      if (!response.ok && !responseData.token && !responseData.message) {
+        console.log({ responseData });
+        throw new Error(responseData?.error?.message || "Something went wrong");
+      }
+      return responseData;
+    } else {
+      // Handle non-JSON responses (e.g., plain text)
+      const text = await response.text();
+      if (!response.ok) {
+        throw new Error(text || "Something went wrong");
+      }
+      return text;
+    }
+  } catch (error: unknown) {
+    const errorMessage =
+      (error as CustomError)?.message || "An unexpected error occurred";
+    if (errorMessage === "Invalid or expired token") {
+      // Handle token expiration (e.g., logout)
+      // logoutAction();
+    }
+    return {
+      error: {
+        title: "Error!",
+        description: errorMessage,
+        duration: 3000,
+      },
+    };
+  }
+}
