01f57cd628d970441a8917da8cf8a682e2c35e79
betigirma <beti.3170.bassist@gmail.com>
2025-04-09 08:49:03 -0300

    added this in user folder under (dashboard) for a route.

---
+++ app/(dashboard)/user/about/page.tsx
@@ -0,0 +1,12 @@
+'use server';
+import AboutPageView from '@/components/views/about-page';
+
+function About() {
+  return (
+    <>
+      <AboutPageView />
+    </>
+  );
+}
+
+export default About;
---
+++ app/(dashboard)/user/blogs/page.tsx
@@ -0,0 +1,12 @@
+'use server';
+import BlogsPageView from '@/components/views/about-page';
+
+function Blogs() {
+  return (
+    <>
+      <BlogsPageView />
+    </>
+  );
+}
+
+export default Blogs;
