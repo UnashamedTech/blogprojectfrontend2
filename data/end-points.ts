56011e53ae7eee6cfd3da987f597800e68a9914f
betigirma <beti.3170.bassist@gmail.com>
2025-05-28 15:07:37 +0300

    endpoints

---
+++ data/end-points.ts
@@ -0,0 +1,7 @@
+export const endPoints = {
+    auth: 'auth/google-local',
+    adminAccount: 'admin/account',
+    adminUser: 'admin/user',
+    allUsers: 'admin/user/all',
+    userProfile: 'admin/user',
+};
