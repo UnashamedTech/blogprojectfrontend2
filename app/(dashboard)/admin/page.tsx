55519710233cd20e23cc349ddc9e238139fafe00
betigirma <beti.3170.bassist@gmail.com>
2025-05-28 14:49:06 +0300

    this is admin's page and it checks authorization before showing the admin view.

---
+++ app/(dashboard)/admin/page.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import AdminView from '@/components/views/admin/dashboard';
+import LoginPageCard from '@/components/login-page/login-page-card';
+import { getCookie } from 'cookies-next';
+import { setAuthCookie } from '@/actions/auth/auth';
+import { useSearchParams } from 'next/navigation';
+
+function AdminPage() {
+  const [authorized, setAuthorized] = useState<boolean | null>(null);
+  const [token, setToken] = useState<string | null>(null);
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+const checkAuthorization = () => {
+  const userProfileCookie = getCookie('user-profile') as string | undefined;
+  if (!userProfileCookie) {
+    setAuthorized(false);
+    return;
+  }
+
+  try {
+    const profile = JSON.parse(userProfileCookie);
+    if (profile.role === 'OWNER') {
+      setAuthorized(true);
+    } else {
+      setAuthorized(false);
+    }
+  } catch (err) {
+    console.error('Failed to parse user profile:', err);
+    setAuthorized(false);
+  }
+};
+
+
+    const initialize = async () => {
+      const tokenFromUrl = searchParams?.get('token');
+
+      if (tokenFromUrl) {
+        setToken(tokenFromUrl);
+
+        const response = await setAuthCookie(tokenFromUrl);
+
+        if (response === true) {
+          checkAuthorization(); 
+        } else {
+          setAuthorized(false);
+        }
+      } else {
+        checkAuthorization();
+      }
+    };
+
+    initialize();
+  }, [searchParams]);
+
+  if (authorized === null) return <div>Loading...</div>;
+
+  if (!authorized) {
+    return (
+      <div className="w-full p-8 flex justify-center">
+        <div className="max-w-md w-full">
+          <LoginPageCard />
+        </div>
+      </div>
+    );
+  }
+
+  return <AdminView />;
+}
+
+export default AdminPage;
