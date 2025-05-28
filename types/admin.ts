e49e328649a44c2a97483f63c6392e3a5f1c909c
betigirma <beti.3170.bassist@gmail.com>
2025-05-28 15:10:16 +0300

    type defination for admin

---
+++ types/admin.ts
@@ -0,0 +1,14 @@
+export interface LayoutProps {
+    children: React.ReactNode;
+}
+export interface Admin {
+    id: string;
+    name: string;
+    age: number;
+    gender: string;
+    email: string;
+    phoneNumber: string;
+    location: string;
+    status: string;
+    profileImage: string;
+}
