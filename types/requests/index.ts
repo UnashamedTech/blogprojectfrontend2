9db6720c1880e4c6bba4bbc6083f62a209dd3369
betigirma <beti.3170.bassist@gmail.com>
2025-06-18 15:17:34 -0700

    add and update category props added.

--- types/requests/index.ts
+++ types/requests/index.ts
@@ -2,3 +2,13 @@ export interface inviteUserProps {
   name: string;
   email: string;
 }
+
+export interface addCategoryProps {
+  CategoryName: string;
+  Description: string;
+}
+
+export type updateCategoryProps = {
+  CategoryName: string;
+  Description: string;
+};
