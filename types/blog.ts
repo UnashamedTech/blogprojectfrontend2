a9b0ae2c962093468c3ce17c9a6c7523aac0a451
betigirma <beti.3170.bassist@gmail.com>
2025-06-19 16:35:34 -0700

    type for blogs

--- types/blog.ts
+++ types/blog.ts
@@ -1,1 +1,17 @@
-// type of blog related
+import type React from 'react';
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface Blogs {
+    id: string | number;
+ Title?: string;
+  Category?: string;
+  location?: string;
+  CreatedDate?: string;
+  Comments?: string;
+  Likes?: string;
+  CommentsAndLikes?: string | number;
+}
+
+export type SortField = 'Title' | 'Category' | 'CreatedDate' | 'location' | 'Comments' | 'Likes';
